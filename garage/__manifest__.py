1dabe2cc0efcd47acc73a748ffed0d808e805215
olmoscristianraul <olmoscristianraul@gmail.com>
2021-04-16 20:41:55 +0000

    primer commit

---
+++ garage/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Garage",
+
+    'summary': "Control de Garage",
+
+    'description': """
+        Modulo para el control de Garage
+    """,
+
+    'author': "HC Sinergia",
+    'website': "http://www.hcsinergia.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Administration',
+    'version': '1.0',
+
+    # any module necessary for this one to work correctly
+    'depends': [],
+
+    # always loaded
+    'data': [],
+    # only loaded in demonstration mode
+    # 'demo': [
+    #     'demo/demo.xml',
+    # ],
+}
---
+++ garage/models/__init__.py
@@ -0,0 +1,2 @@
+# -*- coding: utf-8 -*-
+from . import employee, salary_movement, products, res_country
