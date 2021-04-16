7ae5d10fe53b022b9fc34546811f5659e2b0c1e6
Cristian R. Olmos <olmoscristianraul@gmail.com>
2021-04-16 19:20:05 -0300

    Update __manifest__.py

--- garage/__manifest__.py
+++ garage/__manifest__.py
@@ -18,10 +18,15 @@
     'version': '1.0',
 
     # any module necessary for this one to work correctly
-    'depends': [],
+    'depends': [
+        #'hr', 
+        #'contacts'
+    ],
 
     # always loaded
-    'data': [],
+    'data': [ 
+    #    'security/ir.model.access.csv',
+    ],
     # only loaded in demonstration mode
     # 'demo': [
     #     'demo/demo.xml',
