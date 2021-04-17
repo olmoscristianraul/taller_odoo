bc906fcab4423999dc23492bbd34de532373b7a4
olmoscristianraul <olmoscristianraul@gmail.com>
2021-04-17 22:44:14 +0000

    cambios en models agregro la clase coche, mantenimiento y sus relaciones

--- garage/models/models.py
+++ garage/models/models.py
@@ -10,38 +10,38 @@ class Aparcamiento(models.Model):
     
 
     #Relaciones
-    #coche_ids =fields.One2many('garage.coche','aparcamiento_id', string='Coches')
+    coche_ids =fields.One2many('garage.coche','aparcamiento_id', string='Coches')
 
 
-# class coche(models.Model):
-#     _name = 'garage.coche'
-#     _description = 'Permite definir las caracteristicas de un coche'
-#     _order = 'name'
+class coche(models.Model):
+    _name = 'garage.coche'
+    _description = 'Permite definir las caracteristicas de un coche'
+    _order = 'name'
 
-#     name = fields.Char('Matricula', required=True, size=7)
-#     #plazas = fields.Integer(string='Plazas', required=True)
-#     modelo =  fields.Char(string='Modelo', required=True)
-#     construido = fields.Date(string='Fecha de construcción')
-#     consumo = fields.Float('Consumo',(4,1), default=0.0, help='Consumo promedio cada 100 kms')
-#     #annos = fields.Integer('Años', compute='_get_annos', store=True)#no se requiere strore = true en este ejemplo
-#     annos = fields.Integer('Años', compute='_get_annos')
-#     descripcion = fields.Text('Descripción')
+    name = fields.Char('Matricula', required=True, size=7)
+    #plazas = fields.Integer(string='Plazas', required=True)
+    modelo =  fields.Char(string='Modelo', required=True)
+    construido = fields.Date(string='Fecha de construcción')
+    consumo = fields.Float('Consumo',(4,1), default=0.0, help='Consumo promedio cada 100 kms')
+    #annos = fields.Integer('Años', compute='_get_annos', store=True)#no se requiere strore = true en este ejemplo
+    annos = fields.Integer('Años', compute='_get_annos')
+    descripcion = fields.Text('Descripción')
 
-#     #aparcamiento_id = fields.Many2one('garage.aparcamiento', string='Aparcamiento')
-#     #aparcamiento_ids = fields.Many2many('garage.mantenimiento', string='Mantenimientos')
+    #aparcamiento_id = fields.Many2one('garage.aparcamiento', string='Aparcamiento')
+    #aparcamiento_ids = fields.Many2many('garage.mantenimiento', string='Mantenimientos')
 
-#     @api.depends('construido')
-#     def _get_annos(self):
-#         for coche in self:
-#             coche.annos = 0
+    @api.depends('construido')
+    def _get_annos(self):
+        for coche in self:
+            coche.annos = 0
 
-# class mantenimiento(models.Model):
-#     _name = 'garage.mantenimiento'
-#     _description = 'Permite definir el mantenimiento rutinario de un coche'
-#     _order = 'fecha'
+class mantenimiento(models.Model):
+    _name = 'garage.mantenimiento'
+    _description = 'Permite definir el mantenimiento rutinario de un coche'
+    _order = 'fecha'
 
-#     fecha = fields.Date('Fecha', required=True, default = fields.date.today())
-#     tipo = fields.Selection(string='Tipo', selection[('l','Lavar'),('r','Revisión'),('m','Mecánica'),('p','Pintura')], default = 'l')
-#     coste = fields.Float('Coste', (8,2), help='Coste total del mantenimiento')
+    fecha = fields.Date('Fecha', required=True, default = fields.date.today())
+    tipo = fields.Selection(string='Tipo', selection[('l','Lavar'),('r','Revisión'),('m','Mecánica'),('p','Pintura')], default = 'l')
+    coste = fields.Float('Coste', (8,2), help='Coste total del mantenimiento')
     
-#     #coche_ids = fields.Many2many('garage.coche', string='Coche')
+    #coche_ids = fields.Many2many('garage.coche', string='Coche')
